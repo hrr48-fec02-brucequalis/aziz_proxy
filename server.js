139b5dab864d5d971ec216edcfe7aa2e25e69dc9
Aziz Bouland <q8boland@gmail.com>
2020-10-09 06:54:38 +0300

    product changes by url /id

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const app = express();
 const port = 3000;
 
 app.use(morgan('dev'));
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/:productId', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
 
 app.listen(port, () => {
   console.log(`proxy server is running at port ${port}`)
